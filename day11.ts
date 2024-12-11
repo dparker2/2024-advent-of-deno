d7c029fabca20145e9d9834c5748c72851ded5f7
dparker2 <crazdave@gmail.com>
2024-12-10 22:45:46 -0800

    stone counts, 75 blinks

--- day11.ts
+++ day11.ts
@@ -1,22 +1,42 @@
 // Template
 import { readInput } from "./util.ts";
 
+function addTo(object: Record<string, number>, stone: string, count: number) {
+  if (stone in object) object[stone] += count;
+  else object[stone] = count;
+}
+
 function main(input: string): [number, number] {
-  let stones = input.split(" ").map(Number);
-  for (let i = 0; i < 25; i++) {
-    stones = stones.flatMap((stone) => {
-      const stoneStr = stone.toString();
-      if (stone === 0) return [1];
-      if (stoneStr.length % 2 === 0)
-        return [
-          Number(stoneStr.slice(0, stoneStr.length / 2)),
-          Number(stoneStr.slice(stoneStr.length / 2)),
-        ];
-      return stone * 2024;
-    });
+  let stones = input
+    .split(" ")
+    .reduce<Record<string, number>>((o, num) => ((o[num] = 1), o), {});
+  const answer: number[] = [];
+
+  for (let i = 0; i < 75; i++) {
+    const newStones: Record<string, number> = {};
+    for (const stone in stones) {
+      const count = stones[stone];
+      if (stone === "0") addTo(newStones, "1", count);
+      else if (stone.length % 2 === 0) {
+        const left = stone.slice(0, stone.length / 2);
+        const right = Number(stone.slice(stone.length / 2)).toString();
+        addTo(newStones, left, count);
+        addTo(newStones, right, count);
+      } else {
+        const newstone = (Number(stone) * 2024).toString();
+        addTo(newStones, newstone, count);
+      }
+    }
+    stones = newStones;
+
+    if (i === 24 || i === 74) {
+      let length = 0;
+      for (const stone in stones) length += stones[stone];
+      answer.push(length);
+    }
   }
 
-  return [stones.length, 0];
+  return [answer[0], answer[1]];
 }
 
 if (import.meta.main) {
@@ -28,12 +48,7 @@ if (import.meta.main) {
 // Test
 import { assertEquals } from "jsr:@std/assert@1.0.8";
 
-Deno.test("Part 1: ", () => {
+Deno.test("25 Blinks", () => {
   const [out, _] = main(`125 17`);
   assertEquals(out, 55312);
-});
-
-Deno.test("Part 2: ", () => {
-  const [_, out] = main(``);
-  assertEquals(out, 0);
 });
